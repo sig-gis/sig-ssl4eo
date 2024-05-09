268c056c93bd4bd883dba28bf1dbb4fa4c53d842
John Dilger <johnjdilger@gmail.com>
2024-05-09 13:10:51 -0500

    add sigmoid activation

--- fao_models/models/classification/linear.py
+++ fao_models/models/classification/linear.py
@@ -8,6 +8,7 @@ class LinearClassifier(nn.Module):
         super(LinearClassifier, self).__init__()
         self.num_labels = num_labels
         self.linear = nn.Linear(dim, num_labels)
+        self.sigmoid = nn.Sigmoid()
         self.linear.weight.data.normal_(mean=0.0, std=0.01)
         self.linear.bias.data.zero_()
 
@@ -16,4 +17,7 @@ class LinearClassifier(nn.Module):
         x = x.view(x.size(0), -1)
 
         # linear layer
-        return self.linear(x)
+        x = self.linear(x)
+        if self.num_labels == 1:
+            x = self.sigmoid(x)
+        return x
