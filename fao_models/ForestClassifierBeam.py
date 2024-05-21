7ba3fb6e73ed5e85a906b402694a2e55af839f16
John Dilger <johnjdilger@gmail.com>
2024-05-21 15:19:21 -0500

    Dict2Str func, write to text

--- fao_models/ForestClassifierBeam.py
+++ fao_models/ForestClassifierBeam.py
@@ -1,7 +1,8 @@
+import collections
 from pathlib import PosixPath
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.io import ReadFromCsv, WriteToCsv
+from apache_beam.io import ReadFromCsv, WriteToCsv, WriteToText
 from apache_beam.dataframe.io import read_csv, to_csv
 
 P = "/Users/johndilger/Documents/projects/SSL4EO-S12/fao_models/data/match_dev.csv"
@@ -25,6 +26,57 @@ BANDS = [
 ]
 CROPS = [44, 264, 264, 264, 132, 132, 132, 264, 132, 44, 44, 132, 132]
 PROJECT = "pc530-fao-fra-rss"
+
+import csv
+import io
+import apache_beam as beam
+
+
+# https://github.com/kubeflow/examples/blob/master/LICENSE
+class DictToCSVString(beam.DoFn):
+    """Convert incoming dict to a CSV string.
+
+    This DoFn converts a Python dict into
+    a CSV string.
+
+    Args:
+      fieldnames: A list of strings representing keys of a dict.
+    """
+
+    def __init__(self, fieldnames):
+        super(DictToCSVString, self).__init__()
+
+        self.fieldnames = fieldnames
+
+    def process(self, element, *_args, **_kwargs) -> collections.abc.Iterator[str]:
+        """Convert a Python dict instance into CSV string.
+
+        This routine uses the Python CSV DictReader to
+        robustly convert an input dict to a comma-separated
+        CSV string. This also handles appropriate escaping of
+        characters like the delimiter ",". The dict values
+        must be serializable into a string.
+
+        Args:
+          element: A dict mapping string keys to string values.
+            {
+              "key1": "STRING",
+              "key2": "STRING"
+            }
+
+        Yields:
+          A string representing the row in CSV format.
+        """
+        fieldnames = self.fieldnames
+        filtered_element = {
+            key: value for (key, value) in element.items() if key in fieldnames
+        }
+        with io.StringIO() as stream:
+            writer = csv.DictWriter(stream, fieldnames)
+            writer.writerow(filtered_element)
+            csv_string = stream.getvalue().strip("\r\n")
+
+        yield csv_string
 
 
 class ComputeWordLengthFn(beam.DoFn):
@@ -129,14 +181,17 @@ def dict_to_row(d):
 
 
 beam_options = PipelineOptions()
+cols = ["id", "long", "lat", "prob_label", "pred_label"]
 with beam.Pipeline() as p:
     bdf = (
         p
         | "read input data" >> ReadFromCsv(P)
         | "download imagery" >> beam.ParDo(GetImagery(dst=TMP)).with_output_types(dict)
         | "predict" >> beam.ParDo(Predict(config_path=CONFIG)).with_output_types(dict)
-        | "convert to rows" >> beam.Map(dict_to_row)
-        | "write to csv" >> beam.Map(print)  # WriteToCsv(O)
+        # | "convert to rows" >> beam.Map(dict_to_row)
+        | "to csv str" >> beam.ParDo(DictToCSVString(cols))
+        # | "write to csv" >> WriteToCsv(O)  # beam.Map(print)
+        | "write to csv" >> WriteToText(O, header=",".join(cols))
     )
 
 
