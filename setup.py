0e89c429dc53482d4353ca94bdc746b6d3d97da8
John Dilger <johnjdilger@gmail.com>
2024-06-20 09:56:03 -0500

    init setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fucker",
+    version="0.0.1",
+    url="https://github.com/mypackage.git",
+    author="John Dilger",
+    author_email="johnjdilger@gmail.com",
+    description="Description of my package",
+    packages=find_packages(),
+    # package_dir={"": "fao_models"},
+)
