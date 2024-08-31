a8f99a549e4b849262d840ac05340a84bf8684aa
John Dilger <johnjdilger@gmail.com>
2024-08-31 10:55:34 -0500

    add validate option, add binary acc

--- fao_models/cli.py
+++ fao_models/cli.py
@@ -4,11 +4,12 @@ import torch
 
 from common import load_yml
 from datasets.ssl4eo_dataset import SSL4EO, random_subset
-from main import eval_linear
+from main import eval_linear, validate_linear
 from _types import Config
 
 
-def main(config: str, test: Annotated[bool, typer.Option()] = False):
+def main(config: str, test: Annotated[bool, typer.Option()] = False, 
+         validate: Annotated[bool, typer.Option()]=False):
     args = Config(**load_yml(config))
 
     _data_train = SSL4EO(
@@ -24,23 +25,41 @@ def main(config: str, test: Annotated[bool, typer.Option()] = False):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using: {device}")
     print(f"testing size:{len(_data_test)}")
-    eval_linear(
-        training_data=_data_train,
-        dataset_val=_data_test,
-        arch=args.arch,
-        model_root=args.model_root,
-        avgpool_patchtokens=args.avgpool_patchtokens,
-        patch_size=args.patch_size,
-        n_last_blocks=args.n_last_blocks,
-        lr=args.lr,
-        epochs=args.epochs,
-        batch_size=args.batch_size,
-        checkpoint_key=args.checkpoint_key,
-        checkpoints_dir=args.checkpoints_dir,
-        resume=args.resume,
-        device=device,
-        num_workers=args.num_workers,
-    )
+    if validate:
+        validate_linear(
+            dataset_val=_data_test,
+            arch=args.arch,
+            model_root=args.model_root,
+            avgpool_patchtokens=args.avgpool_patchtokens,
+            patch_size=args.patch_size,
+            n_last_blocks=args.n_last_blocks,
+            lr=args.lr,
+            epochs=args.epochs,
+            batch_size=args.batch_size,
+            checkpoint_key=args.checkpoint_key,
+            checkpoints_dir=args.checkpoints_dir,
+            resume=args.resume,
+            device=device,
+            num_workers=args.num_workers,
+        )
+    else:
+        eval_linear(
+            training_data=_data_train,
+            dataset_val=_data_test,
+            arch=args.arch,
+            model_root=args.model_root,
+            avgpool_patchtokens=args.avgpool_patchtokens,
+            patch_size=args.patch_size,
+            n_last_blocks=args.n_last_blocks,
+            lr=args.lr,
+            epochs=args.epochs,
+            batch_size=args.batch_size,
+            checkpoint_key=args.checkpoint_key,
+            checkpoints_dir=args.checkpoints_dir,
+            resume=args.resume,
+            device=device,
+            num_workers=args.num_workers,
+        )
 
 
 if __name__ == "__main__":
--- fao_models/main.py
+++ fao_models/main.py
@@ -12,6 +12,7 @@ from sklearn.metrics import average_precision_score
 from torchmetrics.classification import BinaryF1Score
 from torchmetrics.classification import BinaryPrecision
 from torchmetrics.classification import BinaryRecall
+from torchmetrics.classification import BinaryAccuracy
 
 from models.dino import utils
 from models._models import get_model
@@ -104,6 +105,7 @@ def validate_network(val_loader, model, linear_classifier, n, avgpool, device, a
         acc2 = BinaryF1Score()(_output, _target)
         acc3 = BinaryPrecision()(_output, _target)
         acc4 = BinaryRecall()(_output, _target)
+        acc5 = BinaryAccuracy()(_output, _target)
 
         batch_size = inp.shape[0]
         metric_logger.update(loss=loss.item())
@@ -113,13 +115,15 @@ def validate_network(val_loader, model, linear_classifier, n, avgpool, device, a
         metric_logger.meters["binary_f1_score"].update(acc2.item(), n=batch_size)
         metric_logger.meters["binary_precision_score"].update(acc3.item(), n=batch_size)
         metric_logger.meters["binary_recall_score"].update(acc4.item(), n=batch_size)
+        metric_logger.meters["binary_acc_score"].update(acc5.item(), n=batch_size)
 
     print(
-        f"* Average Precision: {metric_logger.average_precision_score.global_avg:.3f} "
-        f"* F1: {metric_logger.binary_f1_score.global_avg:.3f} "
-        f"* Precision {metric_logger.binary_precision_score.global_avg:.3f} "
-        f"* Recall {metric_logger.binary_recall_score.global_avg:.3f} "
-        f"* loss: {metric_logger.loss.global_avg:.3f} "
+        f"* Average Precision: {metric_logger.average_precision_score.global_avg:.3f} \n"
+        f"* F1: {metric_logger.binary_f1_score.global_avg:.3f} \n"
+        f"* Precision {metric_logger.binary_precision_score.global_avg:.3f} \n"
+        f"* Recall {metric_logger.binary_recall_score.global_avg:.3f} \n"
+        f"* Accuracy: {metric_logger.binary_acc_score.global_avg:.3f} \n"
+        f"* loss: {metric_logger.loss.global_avg:.3f} \n"
     )
     return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
@@ -257,6 +261,91 @@ def eval_linear(
                 "best_acc": best_acc,
             }
             torch.save(save_dict, os.path.join(checkpoints_dir, "checkpoint.pth.tar"))
+    print(
+        "Training of the supervised linear classifier on frozen features completed.\n"
+        "Top-1 test accuracy: {acc:.1f}".format(acc=best_acc)
+    )
+
+def validate_linear(
+    dataset_val: torch.utils.data.Dataset,
+    arch: Literal["vit_small"],
+    device: str,
+    model_root: str | Path,
+    avgpool_patchtokens: bool,
+    patch_size: int,
+    n_last_blocks: int,
+    lr: float,
+    batch_size: int,
+    checkpoints_dir: str | Path,
+    resume: bool,
+    epochs: int,
+    num_workers: int,
+    checkpoint_key: str = "teacher",
+):
+    val_loader = DataLoader(
+        dataset_val,
+        batch_size=batch_size,
+        shuffle=False,
+        drop_last=True,
+        num_workers=num_workers,
+    )
+
+    model, linear_classifier = get_model(
+        model_name="linear-dino",
+        model_root=model_root,
+        checkpoint_key=checkpoint_key,
+        arch=arch,
+        patch_size=patch_size,
+        n_last_blocks=n_last_blocks,
+        avgpool_patchtokens=avgpool_patchtokens,
+    )
+    # attach to gpu/cpu
+    model = model.to(device)
+    linear_classifier = linear_classifier.to(device)
+    print("model is cuda?", next(model.parameters()).is_cuda)
+    print("classifier is cuda?", next(linear_classifier.parameters()).is_cuda)
+
+    optimizer = torch.optim.SGD(
+        linear_classifier.parameters(),
+        lr,  # linear scaling rule
+        momentum=0.9,
+        weight_decay=0,  # we do not apply weight decay
+    )
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, epochs, eta_min=0)
+    # Optionally resume from a checkpoint
+    to_restore = {"epoch": 0, "best_acc": 0.0}
+    if resume:
+        utils.restart_from_checkpoint(
+            os.path.join(checkpoints_dir, "checkpoint.pth.tar"),
+            run_variables=to_restore,
+            state_dict=linear_classifier,
+            optimizer=optimizer,
+            scheduler=scheduler,
+        )
+    start_epoch = to_restore["epoch"]
+    best_acc = to_restore["best_acc"]
+    os.makedirs(checkpoints_dir, exist_ok=True)
+
+    test_stats = validate_network(
+        val_loader,
+        model,
+        linear_classifier,
+        n_last_blocks,
+        avgpool_patchtokens,
+        device=device,
+        arch=arch,
+    )
+    print(
+        f"Accuracy of the network on the {len(dataset_val)} test images: {test_stats['average_precision_score']:.1f}%"
+    )
+    best_acc = max(best_acc, test_stats["average_precision_score"])
+    print(f"Max accuracy so far: {best_acc:.2f}%")
+    log_stats = {
+        **{f"test_{k}": v for k, v in test_stats.items()},
+    }
+    if utils.is_main_process():
+        with (Path(checkpoints_dir) / "val_log.txt").open("a") as f:
+            f.write(json.dumps(log_stats) + "\n")
     print(
         "Training of the supervised linear classifier on frozen features completed.\n"
         "Top-1 test accuracy: {acc:.1f}".format(acc=best_acc)
